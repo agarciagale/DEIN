62a97d8c10c1043720eb80248b4085bc39b7335d
Adrián <agarciagale@gmail.com>
2023-11-07 17:50:34 +0100

    mevoy que juega el barsa

--- Introducción de Datos de Empleados/MainWindow.xaml.cs
+++ Introducción de Datos de Empleados/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,53 @@ namespace Introducción_de_Datos_de_Empleados
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GuardarEmpleado(object sender, RoutedEventArgs e)
+        {
+            
+            Empleado empleado = new Empleado(Nombre.Text, Apellidos.Text, Email.Text, Telefono.Text);
+            tablaEmpleados.Items.Add(empleado);
+        }
+
+        private void CargarFoto(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(openFileDialog.FileName);
+                bitmap.EndInit();
+                UserImage.Source = bitmap;
+            }
+        }
+    }
+
+
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+
+        public Empleado(string nombre, string apellidos, string email, string telefono)
+        {
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+            this.Email = email;
+            this.Telefono = telefono;
+        }
+
+        private void comprobarDatos(Empleado empleado)
+        {
+            foreach (PropertyInfo campo in empleado.GetType().GetProperties())
+            {
+                
+            }
         }
     }
 }
