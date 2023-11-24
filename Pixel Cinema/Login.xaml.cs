2a48777830e3c332fe3c965a9e5e6e923fdba121
Adrián <agarciagale@gmail.com>
2023-11-24 21:00:31 +0100

    ultimo retoque

--- Pixel Cinema/Login.xaml.cs
+++ Pixel Cinema/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +25,42 @@ namespace Pixel_Cinema
             InitializeComponent();
         }
 
+        public class NavigationHelper
+        {
+            public void NavigateToURL(string url)
+            {
+                try
+                {
+                    Process.Start(url);
+                }
+                catch (Exception ex)
+                {
+                    // Handle any exceptions, such as if the URL is invalid or there's no associated application to open the URL
+                    Console.WriteLine("Error navigating to URL: " + ex.Message);
+                }
+            }
+        }
+
         public void OpenMainWindow(object sender, RoutedEventArgs e)
         {
             MainWindow mainWindow = new MainWindow();
             Window.GetWindow(this).Close();
             mainWindow.Show();
+        }
+
+        public void TerminosYCondiciones(object sender, RoutedEventArgs e)
+        {
+            NavigationHelper navigationHelper = new NavigationHelper();
+
+            // Call the method with the desired URL
+            string desiredURL = "https://www.youtube.com/watch?v=5992zvskJz4";
+            navigationHelper.NavigateToURL(desiredURL);
+        }
+
+        public void PasswordOlvidada(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("F", "xd", MessageBoxButton.OK, MessageBoxImage.Error);
+
         }
     }
 }
